86228dfeb2fc8c57196e3df7f8267c7bafa4fd99
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2019-06-12 16:50:57 -0700

    fix: Safari ScrollTop (original PR is fix: Safari scrollTop #243) 🐛

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-import config, { ERRORS } from './config';
+import config, {ERRORS} from './config'
 
 /**
  * console warning in production
@@ -76,7 +76,7 @@ export const loopTracker = {
 export const scrollBarStorage = {
   key: '_infiniteScrollHeight',
   getScrollElm(elm) {
-    return elm === window ? document.documentElement : elm;
+    return elm === window ? (document.scrollElement || document.documentElement) : elm;
   },
   save(elm) {
     const target = this.getScrollElm(elm);
